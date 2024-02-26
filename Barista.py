7397d675c3d579965005bed54e1ab62ee77ea747
Priscillah matipe <81345790+priscillahmatipe@users.noreply.github.com>
2024-02-26 14:48:38 +0300

    Add files via upload

---
+++ Barista.py
@@ -0,0 +1,11 @@
+#Lets build a robot barista
+
+print("Hello,Welcome to Pashah Coffee Barista!!!")
+name=input("What is your name?\n") #e assigned a variable
+print("Hello"+name+",Thankyou so much for coming in today.\n")
+menu="black coffee,espresso,latte,cappucino"
+print(name+",what would you like from our menu today?Here is what we are serving.\n"+menu)
+order=input()
+print("Sounds good""\t"+name+ "we will have that"  +order+  "ready for you in a moment.")
+
+
